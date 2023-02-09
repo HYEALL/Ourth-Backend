40468630a9d8a168d579c215b6fbea6845b8af4d
YehyeokBang <qkddpgur318@gmail.com>
2023-02-09 22:32:12 +0900

    feat: init project

---
+++ src/main/java/gdsc/skhu/ourth/OurthApplication.java
@@ -0,0 +1,13 @@
+package gdsc.skhu.ourth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OurthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gdsc/skhu/ourth/OurthApplicationTests.java
@@ -0,0 +1,13 @@
+package gdsc.skhu.ourth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
