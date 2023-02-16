ed74a860a21aa6c973598d40cdb7de1621aea73f
성은 <kwakse0720@gmail.com>
2023-02-16 22:27:01 +0900

    feat. Add User entity

---
+++ src/main/java/gdsc/skhu/ourth/domain/entity/User.java
@@ -0,0 +1,77 @@
+package gdsc.skhu.ourth.domain.entity;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.BatchSize;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Getter
+@Entity
+@Table(name = "user")
+public class User implements UserDetails {
+    // 유저 아이디
+    @Id
+    @Column(name = "id", unique = true, nullable = false)
+    private String id;
+
+    // 유저 비밀번호
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    // 유저 네임
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    //
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Builder.Default
+    @BatchSize(size = 1)
+    private List<String> roles = new ArrayList<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
