da1ba6be60c6b6bc18344b9767bdc8c5e31d27c6
YehyeokBang <qkddpgur318@gmail.com>
2023-02-18 14:03:02 +0900

    feat: Add User, Mission, UserMission Repository

---
+++ src/main/java/gdsc/skhu/ourth/repository/MissionRepository.java
@@ -0,0 +1,10 @@
+package gdsc.skhu.ourth.repository;
+
+import gdsc.skhu.ourth.domain.Mission;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissionRepository extends JpaRepository<Mission, Long> {
+
+
+
+}
