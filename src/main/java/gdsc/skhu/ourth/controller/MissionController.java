c638be4f6c279acf8a6f9d5f543aa47904f3f983
YehyeokBang <qkddpgur318@gmail.com>
2023-02-18 14:02:28 +0900

    feat: Add User, Mission, UserMission Controller

---
+++ src/main/java/gdsc/skhu/ourth/controller/MissionController.java
@@ -0,0 +1,23 @@
+package gdsc.skhu.ourth.controller;
+
+import gdsc.skhu.ourth.domain.dto.MissionDTO;
+import gdsc.skhu.ourth.service.MissionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class MissionController {
+
+    private final MissionService missionService;
+
+    // 미션 리스트 확인하기 - 관리자
+    @GetMapping("/mission")
+    public List<MissionDTO> missionList() {
+        return missionService.missionList();
+    }
+
+}
---
+++ src/main/java/gdsc/skhu/ourth/controller/UserController.java
@@ -0,0 +1,46 @@
+package gdsc.skhu.ourth.controller;
+
+import gdsc.skhu.ourth.domain.dto.*;
+import gdsc.skhu.ourth.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    // 메인 페이지 - 누구나
+    @GetMapping("/main")
+    public String main() {
+        return "메인 페이지";
+    }
+
+    // 로그인 - 누구나
+    @PostMapping("/login")
+    public TokenDTO login(@RequestBody UserDTO.Login dto) {
+        return userService.login(dto);
+    }
+
+    // 회원가입 - 누구나
+    @PostMapping("/join")
+    public ResponseEntity<String> join(@Valid @RequestBody UserDTO.SignUp dto) throws Exception {
+        return ResponseEntity.ok(userService.signUp(dto).toString() + " 회원가입 완료");
+    }
+
+
+    // 토큰 값으로 내 정보 확인 - 유저
+    @GetMapping("/user")
+    public UserInfoDTO userInfo(Principal principal) {
+        return userService.userInfo(principal);
+    }
+
+}
