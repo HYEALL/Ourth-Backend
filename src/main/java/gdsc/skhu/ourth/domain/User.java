3c3f5cfdbeae8a5f0c7dee4547fc93b9f3fb42b9
YehyeokBang <qkddpgur318@gmail.com>
2023-02-18 14:00:08 +0900

    feat: Add User, Mission, UserMission Entity

---
+++ src/main/java/gdsc/skhu/ourth/domain/User.java
@@ -0,0 +1,101 @@
+package gdsc.skhu.ourth.domain;
+
+import gdsc.skhu.ourth.domain.dto.UserInfoDTO;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class User extends BaseTimeEntity implements UserDetails {
+
+    // 유저 정보를 저장하는 테이블
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id", updatable = false, unique = true)
+    private Long id; // 고유 번호
+
+    @Column(unique = true)
+    private String email; // 이메일
+
+    private String password; // 비밀번호
+
+    private String username; // 유저 이름
+
+    private String region; // 거주 지역
+
+    private Long point; // 기여 포인트
+
+    @OneToMany(mappedBy = "user")
+    private List<UserMission> userMissions = new ArrayList<>(); // 유저 주간 미션 목록
+
+    // 권한 목록, @ElementCollection: 해당 필드가 컬렉션 객체임을 JPA에게 알려줌
+    @ElementCollection(fetch = FetchType.EAGER) // 연관된 엔티티 즉시 로딩
+    @Builder.Default
+    private List<String> roles = new ArrayList<>();
+
+    public UserInfoDTO toDTO() {
+        return UserInfoDTO.builder()
+                .id(id)
+                .email(email)
+                .username(username)
+                .region(region)
+                .point(point)
+                .userMissions(userMissions.stream()
+                        .map(UserMission::toDTO)
+                        .collect(Collectors.toList()))
+                .build();
+    }
+
+    // 계정의 권한 목록을 리턴
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+
+    // Principal 객체로 이메일 알아내기 위해 오버라이딩
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    public void encodePassword(PasswordEncoder passwordEncoder){
+        this.password = passwordEncoder.encode(password);
+    }
+
+}
---
+++ src/main/java/gdsc/skhu/ourth/domain/UserMission.java
@@ -0,0 +1,49 @@
+package gdsc.skhu.ourth.domain;
+
+import gdsc.skhu.ourth.domain.dto.UserMissionDTO;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Builder
+@Getter
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class UserMission extends BaseTimeEntity {
+
+    // 유저에게 주어진 미션과 완료 여부를 저장하는 테이블
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_mission_id")
+    private Long id;
+
+    @ManyToOne(targetEntity = User.class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user; // N:1, 유저
+
+    @ManyToOne(targetEntity = Mission.class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "mission_id")
+    private Mission mission; // 미션 내용
+
+    @Column(columnDefinition = "boolean default false")
+    private Boolean status; // 미션 완료 여부
+
+    public void setCleared(Boolean status) {
+        this.status = status;
+    }
+
+    public UserMissionDTO toDTO() {
+        return UserMissionDTO.builder()
+                .id(id)
+                .user(user)
+                .mission(mission)
+                .status(status)
+                .text(mission.toDTO().getText())
+                .point(mission.toDTO().getPoint())
+                .build();
+    }
+}
