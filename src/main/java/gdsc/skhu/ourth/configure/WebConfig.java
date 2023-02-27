e110baaab994920be2e4cd32ccc1c43d519168cc
YehyeokBang <qkddpgur318@gmail.com>
2023-02-27 15:50:37 +0900

    feat: Add WebConfig Bean

--- src/main/java/gdsc/skhu/ourth/configure/WebConfig.java
+++ src/main/java/gdsc/skhu/ourth/configure/WebConfig.java
@@ -1,22 +1,31 @@
 package gdsc.skhu.ourth.configure;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
-    public static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
-
-    @Override
-    public void addCorsMappings(final CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedOriginPatterns("http://localhost:3000")
-                .allowedHeaders("*")
-                .maxAge(3600)
-                .allowedMethods(ALLOWED_METHOD_NAMES.split(","))
-                .exposedHeaders(HttpHeaders.LOCATION);
+public class WebConfig
+{
+    @Bean
+    public WebMvcConfigurer corsConfigurer()
+    {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedHeaders(HttpMethod.GET.name(),
+                                HttpMethod.PATCH.name(),
+                                HttpMethod.POST.name(),
+                                HttpMethod.DELETE.name(),
+                                HttpMethod.OPTIONS.name())
+                        .allowCredentials(true)
+                        .maxAge(3600)
+                        .allowedMethods("*");
+            }
+        };
     }
 }
