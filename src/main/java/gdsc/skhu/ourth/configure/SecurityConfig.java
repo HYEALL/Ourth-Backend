09f3ec1bd2dc06f5677a1f3e43074072caf97521
YehyeokBang <qkddpgur318@gmail.com>
2023-03-02 16:30:48 +0900

    feat: https://ourth.duckdns.org 오리진 허용

--- src/main/java/gdsc/skhu/ourth/configure/SecurityConfig.java
+++ src/main/java/gdsc/skhu/ourth/configure/SecurityConfig.java
@@ -71,7 +71,7 @@ public class SecurityConfig {
     public CorsConfigurationSource configurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOriginPatterns(List.of("http://localhost:3000/", "https://ourth-frontend.vercel.app/"));
+        configuration.setAllowedOriginPatterns(List.of("http://localhost:3000/", "https://ourth.duckdns.org/"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of("Access-Control-Allow-Credentials", "Authorization", "Set-Cookie"));
