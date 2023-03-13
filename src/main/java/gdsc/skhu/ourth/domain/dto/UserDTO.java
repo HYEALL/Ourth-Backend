42d0a40999a5631e9fb858946debde8fa6592cdc
YehyeokBang <qkddpgur318@gmail.com>
2023-03-13 21:09:54 +0900

    feat: Add RefreshToken logic and Logout

--- src/main/java/gdsc/skhu/ourth/domain/dto/UserDTO.java
+++ src/main/java/gdsc/skhu/ourth/domain/dto/UserDTO.java
@@ -4,7 +4,6 @@ package gdsc.skhu.ourth.domain.dto;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import gdsc.skhu.ourth.domain.School;
 import gdsc.skhu.ourth.domain.User;
-import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -20,24 +19,18 @@ public class UserDTO {
     @Builder
     @AllArgsConstructor
     public static class RequestSignUp {
-        @NotBlank(message = "이메일을 입력해주세요")
+
         private String email;
 
-        @NotBlank(message = "비밀번호를 입력해주세요")
-        //@Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{4,20}$",
-        //       message = "비밀번호는 8~30 자리이면서 1개 이상의 알파벳, 숫자, 특수문자를 포함해야합니다.") // 아직 안정함
         private String password;
 
-        @NotBlank(message = "비밀번호(확인)를 입력해주세요")
         private String checkedPassword;
 
-        @NotBlank(message = "닉네임을 입력해주세요")
         private String username;
 
         @JsonIgnore
         private School school;
 
-        @NotBlank(message = "거주 지역을 입력해주세요")
         private String schoolName;
 
         // 회원가입시 0포인트부터 시작
