77c8041998bb344dee6c2256c81b5762eeb322cd
YehyeokBang <qkddpgur318@gmail.com>
2023-03-21 17:45:31 +0900

    fix: firebase secret key

--- src/main/java/gdsc/skhu/ourth/configure/FirebaseConfig.java
+++ src/main/java/gdsc/skhu/ourth/configure/FirebaseConfig.java
@@ -4,12 +4,13 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.FirebaseAuth;
+import org.apache.tomcat.util.json.ParseException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
@@ -17,8 +18,7 @@ public class FirebaseConfig {
     // firebase init
     @Bean
     public FirebaseApp firebaseApp() throws IOException {
-        ClassPathResource resource = new ClassPathResource("firebase.json");
-        FileInputStream firebaseKey = new FileInputStream(resource.getFile());
+        InputStream firebaseKey = new ClassPathResource("firebase.json").getInputStream();
 
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(firebaseKey))
